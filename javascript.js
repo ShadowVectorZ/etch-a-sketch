52a14e549d483795175a2bc61b3c00a99e7d2837
ShadowVectorZ <rayaudet19@gmail.com>
2024-02-07 11:01:14 -0500

    connected func that changes grid size to button

--- javascript.js
+++ javascript.js
@@ -1,15 +1,34 @@
 const container= document.querySelector('#container')
 
 let rowNumber
-rowNumber=17
-
+rowNumber=5
 let total=rowNumber*(rowNumber+1)
+
+let createGrid= function(){
+
+
 
 for (let i=0;i<total;i++){
     const squareDiv=document.createElement('div')
     squareDiv.classList.add('squareDiv')
-   if ((i%(rowNumber+1))===0){
+   if ((i%((Number(rowNumber))+1))===0){
     squareDiv.setAttribute('style', ' width: 100%; border: 0;height: 0;')
    }
     container.appendChild(squareDiv)
 }
+}
+
+createGrid()
+
+let sizeChange=function(){
+    while( container.hasChildNodes() ){
+        container.removeChild(container.lastChild);} 
+    rowNumber=0
+    total=0
+   rowNumber=(prompt("What size would you like the grid to be?"))
+   total=(rowNumber)*(((Number(rowNumber))+1))
+
+    createGrid()
+}
+const sizeButton=document.querySelector('#sizeButton')
+sizeButton.addEventListener('click', sizeChange);
