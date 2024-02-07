53d58b93d625c1459c33678104b8ab2716715c9b
ShadowVectorZ <rayaudet19@gmail.com>
2024-02-07 10:18:07 -0500

    added function creates grid

--- javascript.js
+++ javascript.js
@@ -1,13 +1,15 @@
 const container= document.querySelector('#container')
 
 let rowNumber
-rowNumber=16
+rowNumber=18
 
 let total=rowNumber*(rowNumber+1)
 
 for (let i=0;i<total;i++){
     const squareDiv=document.createElement('div')
     squareDiv.classList.add('squareDiv')
-
+   if ((i%(rowNumber+1))===0){
+    squareDiv.setAttribute('style', ' width: 100%; border: 0;height: 0;')
+   }
     container.appendChild(squareDiv)
 }
